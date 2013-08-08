4e1355683bfe19f6345a1bdad4cb3af4a07eba8f
Peter Bartosch <bartoschp@gmail.com>
2013-08-08 09:23:44 -0500

    Scene has been updated and commented

--- include/pbj/scene/scene.h
+++ include/pbj/scene/scene.h
@@ -1,9 +1,8 @@
-///////////////////////////////////////////////////////////////////////////////
-/// \file   pbj/scene.h
-/// \author Benjamin Crist
+////////////////////////////////////////////////////////////////////////////////
+/// \file Z:\Documents\PBJsimple\include\pbj\scene\scene.h
 ///
-/// \brief  pbj::scene::Scene class header.
-
+/// \brief Declares the scene class.
+////////////////////////////////////////////////////////////////////////////////
 #ifndef PBJ_SCENE_SCENE_H_
 #define PBJ_SCENE_SCENE_H_
 
@@ -21,9 +20,37 @@ using std::unordered_map;
 namespace pbj {
 namespace scene {
 
+////////////////////////////////////////////////////////////////////////////////
+/// \class Scene
+///
+/// \brief The Scene class is mostly an Entity container used in the game.
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+/// 
+/// \details This class contains muliple unordered maps call EntityMap that will
+/// 		 map an id number to a given Entity.  The maps are separated by
+/// 		 EntityType.  When new types are added, the new maps will have to be
+/// 		 added and handled.  Currently the UIRoot for the Scene is left as a
+/// 		 global variable, which could probably change but until we have the
+/// 		 user interface stuff down I'm leaving it as is.
+////////////////////////////////////////////////////////////////////////////////
 class Scene
 {
 public:
+
+	////////////////////////////////////////////////////////////////////////////
+	/// \enum EntityType
+	///
+	/// \brief Values that represent EntityType.
+	////////////////////////////////////////////////////////////////////////////
+	enum EntityType
+	{
+		Terrain = 0x01,
+		Player = 0x02,
+		SpawnPoint = 0x04
+	};
+
     Scene();
 	~Scene();
 
@@ -31,13 +58,24 @@ public:
 
 	void draw();
 
-	U32 addEntity(Entity&);
-	void removeEntity(U32);
+	U32 addEntity(const Entity&, EntityType);
+	void removeEntity(U32, EntityType);
 
 private:
+
+	////////////////////////////////////////////////////////////////////////////
+	/// \typedef unordered_map<U32,Entity> EntityMap
+	///
+	/// \brief An alias for an unordered map used for id->Entity
+	////////////////////////////////////////////////////////////////////////////
+	typedef unordered_map<U32,Entity> EntityMap;
+
 	U32 _nextEntityId;
 
-	unordered_map<U32,Entity> _entities;
+	//as we get more Entity types this may have to expand/change entirely
+	EntityMap _spawnPoints;
+	EntityMap _terrain;
+	EntityMap _players;
 
     Scene(const Scene&);
     void operator=(const Scene&);
--- src/pbj/scene/scene.cpp
+++ src/pbj/scene/scene.cpp
@@ -1,52 +1,147 @@
-///////////////////////////////////////////////////////////////////////////////
-/// \file   pbj/scene.cpp
-/// \author Benjamin Crist
+////////////////////////////////////////////////////////////////////////////////
+/// \file Z:\Documents\PBJsimple\src\pbj\scene\scene.cpp
 ///
-/// \brief  Implementations of pbj::scene::Scene functions.
-
+/// \brief Implements the scene class.
+////////////////////////////////////////////////////////////////////////////////
 #include "pbj/scene/scene.h"
 
 namespace pbj {
 namespace scene {
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn Scene::Scene()
+///
+/// \brief Initializes a new instance of the Scene class.
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+////////////////////////////////////////////////////////////////////////////////
 Scene::Scene()
 {
 	_nextEntityId = 0;
-	_entities = unordered_map<U32,Entity>();
+	_spawnPoints = EntityMap();
+	_terrain = EntityMap();
+	_players = EntityMap();
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn Scene::~Scene()
+///
+/// \brief Destroys an instance of the Scene class.
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+////////////////////////////////////////////////////////////////////////////////
 Scene::~Scene()
 {
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void Scene::draw()
+///
+/// \brief Draws the scene
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+/// 
+/// \details This will go through each EntityMap for every drawable EntityType
+/// 		 and draw its members.  In a more general setting this would not be
+/// 		 handled here, I don't think.
+////////////////////////////////////////////////////////////////////////////////
 void Scene::draw()
 {
-	for(unordered_map<U32,Entity>::iterator it = _entities.begin();
-		it != _entities.end();
-		++it)
-	{
+
+	for(EntityMap::iterator it=_terrain.begin();
+		it!=_terrain.end();
+		it++)
 		it->second.draw();
-	}
+
+	for(EntityMap::iterator it=_players.begin();
+		it!=_players.end();
+		it++)
+		it->second.draw();
 
 	//I assume the ui drawing goes like this.
 	ui.draw();
 }
 
-//returns the id of the entity in the map
-U32 Scene::addEntity(Entity& e)
+////////////////////////////////////////////////////////////////////////////////
+/// \fn U32 Scene::addEntity(const Entity& e, EntityType et)
+///
+/// \brief Adds an Entity to the EntityMap for its EntityType
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+///
+/// \param e  A reference to the Entity to add to the scene.
+/// \param et The type of Entity.
+///
+/// \return The id of the Entity in the EntityMap for its type.
+/// 
+/// \sa Entity, Scene::EntityType
+////////////////////////////////////////////////////////////////////////////////
+U32 Scene::addEntity(const Entity& e, EntityType et)
 {
 	U32 ret = _nextEntityId;
+
 	//this seems a little overdone.  Think this could be better.
-	_entities[_nextEntityId].init();
-	_entities[_nextEntityId].setTransform(*(e.getTransform()));
-	_entities[_nextEntityId].setTextureId(e.getTextureId());
-	_nextEntityId++;
+	switch(et)
+	{
+	case Terrain:
+		_terrain[_nextEntityId].init();
+		_terrain[_nextEntityId].setTransform(*(e.getTransform()));
+		_terrain[_nextEntityId].setTextureId(e.getTextureId());
+		_nextEntityId++;
+		break;
+	case Player:
+		_players[_nextEntityId].init();
+		_players[_nextEntityId].setTransform(*(e.getTransform()));
+		_players[_nextEntityId].setTextureId(e.getTextureId());
+		_nextEntityId++;
+		break;
+	case SpawnPoint:
+		_spawnPoints[_nextEntityId].init();
+		_spawnPoints[_nextEntityId].setTransform(*(e.getTransform()));
+		_spawnPoints[_nextEntityId].setTextureId(e.getTextureId());
+		_nextEntityId++;
+		break;
+	default:
+		break;
+	}
+
 	return ret;
 }
 
-void Scene::removeEntity(U32 id)
+////////////////////////////////////////////////////////////////////////////////
+/// \fn void Scene::removeEntity(U32 id, EntityType et)
+///
+/// \brief Removes an Entity from an EntityMap.
+///
+/// \author Peter Bartosch
+/// \date 2013-08-08
+///
+/// \param id The id of the Entity.
+/// \param et The EntityType
+/// 
+/// \details The id for the Entity was returned when it was added with
+/// 		 Scene::addEntity.
+////////////////////////////////////////////////////////////////////////////////
+void Scene::removeEntity(U32 id, EntityType et)
 {
-	_entities.erase(id);
+	switch(et)
+	{
+	case Terrain:
+		_terrain.erase(id);
+		break;
+	case Player:
+		_players.erase(id);
+		break;
+	case SpawnPoint:
+		_spawnPoints.erase(id);
+		break;
+	default:
+		break;
+	}
 }
 
 } // namespace pbj::scene
