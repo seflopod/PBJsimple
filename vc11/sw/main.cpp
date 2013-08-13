b66ed8eb7d2ff51f563b7e65fbf10c638bb186b1
Ben Crist <ben@magicmoremagic.com>
2013-08-12 23:34:51 -0500

    sw create

--- vc11/sw/main.cpp
+++ vc11/sw/main.cpp
@@ -198,8 +198,10 @@ int texture(const std::string& texture, const std::string& filename, const char*
     return 0;
 }
 
+///////////////////////////////////////////////////////////////////////////////
 int create(const std::string& filename)
 {
+    // first open as a regular sqlite3 database and set up sw_sandwich properties with the sandwich ID.
     try
     {
         pbj::db::Db db(filename);
@@ -260,13 +262,12 @@ int create(const std::string& filename)
         return 1;
     }
 
+    // After sw_sandwich_properties is set up, we can open the file using pbj::sw::Sandwich().
     try
     {
         sw = std::shared_ptr<pbj::sw::Sandwich>(new pbj::sw::Sandwich(filename, false));
 
         pbj::db::Stmt table_exists(sw->getDb(), "SELECT name FROM sqlite_master WHERE type = 'table' AND name = ? LIMIT 1");
-
-
 
         table_exists.bind(1, "sw_maps");
         if (!table_exists.step())
@@ -297,129 +298,127 @@ int create(const std::string& filename)
             PBJ_LOG(pbj::VInfo) << "Created table 'sw_map_entities'." << PBJ_LOG_END;
         }
 
-            /*
+        table_exists.reset();
+        table_exists.bind(1, "sw_textures");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_textures\n"
+                             "(\n"
+                             "   id              INTEGER PRIMARY KEY,\n"
+                             "   data            NOT NULL,\n"
+                             "   internal_format INTEGER NOT NULL,\n"
+                             "   srgb            INTEGER NOT NULL,\n"
+                             "   mag_filter      INTEGER NOT NULL,\n"
+                             "   min_filter      INTEGER NOT NULL\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_textures'." << PBJ_LOG_END;
+        }
 
+        table_exists.reset();
+        table_exists.bind(1, "sw_texture_fonts");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_texture_fonts\n"
+                             "(\n"
+                             "   id         INTEGER PRIMARY KEY,\n"
+                             "   texture_id NOT NULL,\n"
+                             "   cap_height INTEGER NOT NULL\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_texture_fonts'." << PBJ_LOG_END;
+        }
 
+        table_exists.reset();
+        table_exists.bind(1, "sw_texture_font_chars");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_texture_font_chars\n"
+                             "(\n"
+                             "   font_id   INTEGER NOT NULL,\n"
+                             "   codepoint INTEGER NOT NULL,\n"
+                             "   tc_x      INTEGER NOT NULL,\n"
+                             "   tc_y      INTEGER NOT NULL,\n"
+                             "   tc_width  INTEGER NOT NULL,\n"
+                             "   tc_height INTEGER NOT NULL,\n"
+                             "   offset_x  INTEGER NOT NULL,\n"
+                             "   offset_y  INTEGER NOT NULL,\n"
+                             "   advance   INTEGER NOT NULL,\n"
+                             "   PRIMARY KEY (font_id, codepoint)\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_texture_font_chars'." << PBJ_LOG_END;
+        }
+        
+        table_exists.reset();
+        table_exists.bind(1, "sw_ui_panel_styles");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_ui_panel_styles\n"
+                             "(\n"
+                             "   id              INTEGER PRIMARY KEY,\n"
+                             "   bg_color_top    INTEGER NOT NULL,\n"
+                             "   bg_color_bottom INTEGER NOT NULL,\n"
+                             "   border_color    INTEGER NOT NULL,\n"
+                             "   margin_color    INTEGER NOT NULL,\n"
+                             "   margin_left     REAL NOT NULL,\n"
+                             "   margin_right    REAL NOT NULL,\n"
+                             "   margin_top      REAL NOT NULL,\n"
+                             "   margin_bottom   REAL NOT NULL,\n"
+                             "   border_left     REAL NOT NULL,\n"
+                             "   border_right    REAL NOT NULL,\n"
+                             "   border_top      REAL NOT NULL,\n"
+                             "   border_bottom   REAL NOT NULL\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_ui_panel_styles'." << PBJ_LOG_END;
+        }
 
+        table_exists.reset();
+        table_exists.bind(1, "sw_ui_button_styles");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_ui_button_styles\n"
+                             "(\n"
+                             "   id             INTEGER PRIMARY KEY,\n"
+                             "   font_id        INTEGER NOT NULL,\n"
+                             "   text_color     INTEGER NOT NULL,\n"
+                             "   text_scale_x   INTEGER NOT NULL,\n"
+                             "   text_scale_y   INTEGER NOT NULL,\n"
+                             "   panel_style_id INTEGER NOT NULL\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_ui_button_styles'." << PBJ_LOG_END;
+        }
 
-CREATE TABLE sw_textures
-(
-      -- Identifies this texture among all other textures in this sandwich
-      id                INTEGER PRIMARY KEY,
-      -- The image data for the texture.  Format depends on the value of `data_format`
-      data              NOT NULL,
-      -- Describes the OpenGL internal format to request for storing the texture on the GPU.
-      internal_format   INTEGER NOT NULL,
-      -- If non-zero, the texture data will be linearized from sRGB when sampling.
-      srgb              INTEGER NOT NULL,
-      -- The sampler filtering mode to use for magnification
-      mag_filter        INTEGER NOT NULL,
-      -- The sampler filtering mode to use for minification
-      min_filter        INTEGER NOT NULL
-);
-
-CREATE TABLE sw_texture_fonts
-(
-      id                INTEGER PRIMARY KEY,
-      texture_id        INTEGER NOT NULL,
-      cap_height        INTEGER NOT NULL
-);
-
-CREATE TABLE sw_texture_font_chars
-(
-      font_id           INTEGER NOT NULL,
-      codepoint         INTEGER NOT NULL,
-      
-      tc_x              INTEGER NOT NULL,
-      tc_y              INTEGER NOT NULL,
-      
-      tc_width          INTEGER NOT NULL,
-      tc_height         INTEGER NOT NULL,
-      
-      offset_x          INTEGER NOT NULL,
-      offset_y          INTEGER NOT NULL,
-      
-      advance           INTEGER NOT NULL,
-      
-      PRIMARY KEY (font_id, codepoint)
-);
-
-CREATE TABLE sw_ui_panel_styles
-(
-      id                INTEGER PRIMARY KEY,
-      bg_color_top      INTEGER NOT NULL,
-      bg_color_bottom   INTEGER NOT NULL,
-      border_color      INTEGER NOT NULL,
-      margin_color      INTEGER NOT NULL,
-      margin_left       REAL NOT NULL,
-      margin_right      REAL NOT NULL,
-      margin_top        REAL NOT NULL,
-      margin_bottom     REAL NOT NULL,
-      border_left       REAL NOT NULL,
-      border_right      REAL NOT NULL,
-      border_top        REAL NOT NULL,
-      border_bottom     REAL NOT NULL
-);
-
-CREATE TABLE sw_ui_button_styles
-(
-      id                INTEGER PRIMARY KEY,
-      font_id           INTEGER NOT NULL,
-      text_color        INTEGER NOT NULL,
-      text_scale_x      REAL NOT NULL,
-      text_scale_y      REAL NOT NULL,
-      panel_style_id    INTEGER NOT NULL
-);
-
-CREATE TABLE sw_window_settings
-(
-      -- Identifies this set of window settings, allowing multiple windows to
-      -- store their settings independently.
-      id                INTEGER NOT NULL,
-      -- When window settings are changed (except when saving new window
-      -- position/size on window close), a new history index is created so
-      -- that the old settings can be restored if the new ones are bad.
-      history_index     INTEGER NOT NULL,
-      
-      -- Determines whether the window is fullscreen or not, and how much
-      -- control the user has over its position/size.
-      --    0  Windowed, resizable
-      --    1  Windowed, not resizable
-      --    2  Windowed, undecorated, resizable
-      --    3  Windowed, undecorated, not resizable
-      --    7  Windowed Fullscreen
-      --    8  Exclusive Fullscreen
-      window_mode       INTEGER NOT NULL,
-      -- If 0, the window should be moved to `position_x`,`position_y` after it
-      -- is created.  Ignored in fullscreen exclusive mode.
-      system_positioned INTEGER NOT NULL,
-      maximized         INTEGER NOT NULL,
-      save_pos_on_close INTEGER NOT NULL,
-      position_x        INTEGER NOT NULL,
-      position_y        INTEGER NOT NULL,
-      size_x            INTEGER NOT NULL,
-      size_y            INTEGER NOT NULL,
-      monitor_index     INTEGER NOT NULL,
-      
-      refresh_rate      INTEGER NOT NULL,
-      v_sync            INTEGER NOT NULL,
-      msaa_level        INTEGER NOT NULL,
-      
-      red_bits          INTEGER NOT NULL,
-      green_bits        INTEGER NOT NULL,
-      blue_bits         INTEGER NOT NULL,
-      alpha_bits        INTEGER NOT NULL,
-      depth_bits        INTEGER NOT NULL,
-      stencil_bits      INTEGER NOT NULL,
-      
-      srgb_capable      INTEGER NOT NULL,
-      use_custom_gamma  INTEGER NOT NULL,
-      custom_gamma      REAL NOT NULL,
-      
-      PRIMARY KEY (id, history_index)
-);
-*/
-
+        table_exists.reset();
+        table_exists.bind(1, "sw_window_settings");
+        if (!table_exists.step())
+        {
+            sw->getDb().exec("CREATE TABLE sw_window_settings\n"
+                             "(\n"
+                             "   id                INTEGER NOT NULL,\n"
+                             "   history_index     INTEGER NOT NULL,\n"
+                             "   window_mode       INTEGER NOT NULL,\n"
+                             "   system_positioned INTEGER NOT NULL,\n"
+                             "   maximized         INTEGER NOT NULL,\n"
+                             "   save_pos_on_close INTEGER NOT NULL,\n"
+                             "   position_x        INTEGER NOT NULL,\n"
+                             "   position_y        INTEGER NOT NULL,\n"
+                             "   size_x            INTEGER NOT NULL,\n"
+                             "   size_y            INTEGER NOT NULL,\n"
+                             "   monitor_index     INTEGER NOT NULL,\n"
+                             "   refresh_rate      INTEGER NOT NULL,\n"
+                             "   v_sync            INTEGER NOT NULL,\n"
+                             "   msaa_level        INTEGER NOT NULL,\n"
+                             "   red_bits          INTEGER NOT NULL,\n"
+                             "   green_bits        INTEGER NOT NULL,\n"
+                             "   blue_bits         INTEGER NOT NULL,\n"
+                             "   alpha_bits        INTEGER NOT NULL,\n"
+                             "   depth_bits        INTEGER NOT NULL,\n"
+                             "   stencil_bits      INTEGER NOT NULL,\n"
+                             "   srgb_capable      INTEGER NOT NULL,\n"
+                             "   use_custom_gamma  INTEGER NOT NULL,\n"
+                             "   custom_gamma      REAL NOT NULL,\n"
+                             "   PRIMARY KEY (id, history_index)\n"
+                             ");");
+            PBJ_LOG(pbj::VInfo) << "Created table 'sw_window_settings'." << PBJ_LOG_END;
+        }
     }
     catch (const pbj::db::Db::error& e)
     {
